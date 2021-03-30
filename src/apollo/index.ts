32b54a946ea09f9527c3efafe3889870b83de454
Regis Baf <regiswareja@gmail.com>
2021-03-31 06:49:38 +0900

    Update index.ts

--- src/apollo/index.ts
+++ src/apollo/index.ts
@@ -2,9 +2,7 @@ import { InMemoryCache, ApolloClient } from "@apollo/client";
 import { typeDefs } from "./TypeDefs";
 import link from "./Middlewares";
 import initCache from "./Cache";
-/**
- * If you are not using no graphql connection connection
- */
+
 let client: ApolloClient<any>;
 
 export const getApolloClient = async (): Promise<ApolloClient<any>> => {
